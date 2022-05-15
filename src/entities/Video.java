f4e46a199507f0b9f9c196bdb8069b449407a88f
rafaelspa <rsdepaula67@gmail.com>
2022-05-15 08:51:04 -0300

    Entities: Created

---
+++ src/entities/Video.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Video extends Lesson {
+	private String url;
+	private int seconds;
+	
+	public Video() {
+		super();
+	}
+	
+	public Video(String url, int seconds) {
+		super();
+		this.url = url;
+		this.seconds = seconds;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+
+	@Override
+	public int duration() {
+		return getSeconds();
+	}
+}

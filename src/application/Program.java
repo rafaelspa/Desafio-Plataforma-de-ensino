23a5c0ecf6662452444714a9fd14cb6e54bea89b
rafaelspa <rsdepaula67@gmail.com>
2022-05-15 08:31:13 -0300

    Program File: start

---
+++ src/application/Program.java
@@ -0,0 +1,20 @@
+/* Module 4 - Chapter 12 - EmployeeOO Challenge - Escola Devsuperior
+ * Prof. Nelio Alves
+ * 
+ * Student: rsdepaula67@gmail.com
+ * 
+ * Description:
+ * Program to exercise principles of OOP, inheritance and polymorphism mostly
+ * Compute the total duration of a course based on the classes Video and Task,
+ * which are subclasses of the class Lesson
+ */
+
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
